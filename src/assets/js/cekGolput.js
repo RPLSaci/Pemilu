181c02aaacb67ee4cdcfd9d4ac91295129b07309
Farrel Athaillah <MoonLMC1212@gmail.com>
2024-09-25 07:54:24 +0000

    a

--- src/assets/js/cekGolput.js
+++ src/assets/js/cekGolput.js
@@ -4,7 +4,7 @@ async function init() {
     let i = 0
     console.log(data)
     data.data.filter((e) => !e.WaktuPemilihan).forEach(element => {
-        if(i < 50) {
+        if(i < 200) {
             document.querySelector("tbody#tbody").innerHTML += `
             <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
--- src/assets/js/ceksuara.js
+++ src/assets/js/ceksuara.js
@@ -1,34 +1,34 @@
-async function init() {
-    let res = await fetch("https://redesigned-lamp-wx969x4q6g6f599x-8080.app.github.dev/api/kandidat/getall");
-    let data = await res.json();
-    let res2 = await fetch("https://redesigned-lamp-wx969x4q6g6f599x-8080.app.github.dev/api/user/getall");
-    let data2 = await res2.json();
+// async function init() {
+//     let res = await fetch("https://redesigned-lamp-wx969x4q6g6f599x-8080.app.github.dev/api/kandidat/getall");
+//     let data = await res.json();
+//     let res2 = await fetch("https://redesigned-lamp-wx969x4q6g6f599x-8080.app.github.dev/api/user/getall");
+//     let data2 = await res2.json();
 
 
-    console.log(data.data);
+//     console.log(data.data);
     
-    document.querySelectorAll("#mpk p.italic").forEach((mpkElement) => {
-        console.log(data2.data.filter((e) => e.kandidatMPK === mpkElement.id).length); // Log the candidate ID
-        const result = data.data.find((candidate) => candidate.tipe === "MPK" && candidate.nomerKandidat === mpkElement.id);
-        if (result) {
-            mpkElement.innerHTML = data2.data.filter((e) => e.kandidatMPK === mpkElement.id).length;
-        } else {
-            mpkElement.innerHTML = "0"; // Or handle the case where the candidate is not found
-        }
-    });
+//     document.querySelectorAll("#mpk p.italic").forEach((mpkElement) => {
+//         console.log(data2.data.filter((e) => e.kandidatMPK === mpkElement.id).length); // Log the candidate ID
+//         const result = data.data.find((candidate) => candidate.tipe === "MPK" && candidate.nomerKandidat === mpkElement.id);
+//         if (result) {
+//             mpkElement.innerHTML = data2.data.filter((e) => e.kandidatMPK === mpkElement.id).length;
+//         } else {
+//             mpkElement.innerHTML = "0"; // Or handle the case where the candidate is not found
+//         }
+//     });
     
-    document.querySelectorAll("#osis p.italic").forEach((osisElement) => {
-        console.log(osisElement.id); // Log the candidate ID
-        const result = data.data.find((candidate) => candidate.tipe === "OSIS" && candidate.nomerKandidat === osisElement.id);
-        console.log(result); // Log the result for debugging
-        if (result) {
-            osisElement.innerHTML = data2.data.filter((e) => e.kandidatOsis === osisElement.id).length;
-        } else {
-            osisElement.innerHTML = "0"; // Handle the case where the candidate is not found
-        }
-    });
-}
+//     document.querySelectorAll("#osis p.italic").forEach((osisElement) => {
+//         console.log(osisElement.id); // Log the candidate ID
+//         const result = data.data.find((candidate) => candidate.tipe === "OSIS" && candidate.nomerKandidat === osisElement.id);
+//         console.log(result); // Log the result for debugging
+//         if (result) {
+//             osisElement.innerHTML = data2.data.filter((e) => e.kandidatOsis === osisElement.id).length;
+//         } else {
+//             osisElement.innerHTML = "0"; // Handle the case where the candidate is not found
+//         }
+//     });
+// }
 
-init();
+// init();
 
-setInterval(init, 5 * 1000)
+// setInterval(init, 5 * 1000)
