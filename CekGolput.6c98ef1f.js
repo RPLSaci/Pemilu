ee735656609814fb8a7a6ef220a909505db7000e
MoonLGH <MoonLMC1212@gmail.com>
2024-09-25 07:55:05 +0000

    Update ready-sch to output generated at 181c02a

--- CekGolput.6c98ef1f.js
+++ CekGolput.6c98ef1f.js
@@ -4,7 +4,7 @@ async function init() {
     let i = 0;
     console.log(data);
     data.data.filter((e)=>!e.WaktuPemilihan).forEach((element)=>{
-        if (i < 50) {
+        if (i < 200) {
             document.querySelector("tbody#tbody").innerHTML += `
             <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
@@ -26,4 +26,4 @@ async function init() {
 }
 init(); // setInterval(init, 5 * 1000)
 
-//# sourceMappingURL=CekGolput.728447a6.js.map
+//# sourceMappingURL=CekGolput.6c98ef1f.js.map
