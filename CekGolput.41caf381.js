9bab24016e48550826990d88289882d7a73a234c
MoonLGH <MoonLGH@users.noreply.github.com>
2024-09-25 00:11:55 +0000

    deploy: d27926cfadf3c0dbbdfec1609497329650b41038

--- CekGolput.41caf381.js
+++ CekGolput.41caf381.js
@@ -1,16 +1,16 @@
-!async function(){let t=await fetch("https://8080-rplsaci-pemiluapi-jfvwph8s12t.ws-us116.gitpod.io/api/user/getall"),e=await t.json(),a=0;console.log(e),e.data.filter(t=>!t.WaktuPemilihan).forEach(t=>{a<50&&(document.querySelector("tbody#tbody").innerHTML+=`
+!async function(){let e=await fetch("https://redesigned-lamp-wx969x4q6g6f599x-8080.app.github.dev/api/user/getall"),t=await e.json(),a=0;console.log(t),t.data.filter(e=>!e.WaktuPemilihan).forEach(e=>{a<50&&(document.querySelector("tbody#tbody").innerHTML+=`
             <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                ${t.username}
+                ${e.username}
             </th>
             <td class="px-6 py-4">
-                ${t.nisn}
+                ${e.nisn}
             </td>
             <td class="px-6 py-4">
-                ${t.password}
+                ${e.password}
             </td>
             <td class="px-6 py-4">
                 <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">SUDAH</a>
             </td>
         </tr>`,a++)})}();
-//# sourceMappingURL=CekGolput.8c6e0405.js.map
+//# sourceMappingURL=CekGolput.41caf381.js.map
--- cekPemilih.d3f76652.js
+++ cekPemilih.d3f76652.js
@@ -1,4 +1,4 @@
-!async function(){let t=await fetch("https://8080-rplsaci-pemiluapi-jfvwph8s12t.ws-us116.gitpod.io/api/user/getall"),a=await t.json(),e=0;console.log(a.data.filter(t=>t.WaktuPemilihan&&t.kandidatMPK)),a.data.filter(t=>t.WaktuPemilihan).sort((t,a)=>new Date(a.WaktuPemilihan)-new Date(t.WaktuPemilihan)).forEach(t=>{if(e<50){let a=new Date(t.WaktuPemilihan);document.querySelector("tbody#tbody").innerHTML+=`
+!async function(){let t=await fetch("https://redesigned-lamp-wx969x4q6g6f599x-8080.app.github.dev/api/user/getall"),a=await t.json(),e=0;console.log(a.data.filter(t=>t.WaktuPemilihan&&t.kandidatMPK)),a.data.filter(t=>t.WaktuPemilihan).sort((t,a)=>new Date(a.WaktuPemilihan)-new Date(t.WaktuPemilihan)).forEach(t=>{if(e<50){let a=new Date(t.WaktuPemilihan);document.querySelector("tbody#tbody").innerHTML+=`
             <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                 ${t.username}
@@ -19,4 +19,4 @@
                 <button onclick="hapus('${t.nisn}','${t.password}')" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Hapus</button>
             </td>
         </tr>`,e++}})}();
-//# sourceMappingURL=cekPemilih.267f75d6.js.map
+//# sourceMappingURL=cekPemilih.d3f76652.js.map
